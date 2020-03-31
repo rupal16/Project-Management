af12860e97aab878a7c0bb0d9f68ac7f63a76815
Vinay Sharma <vinay@geekyants.com>
2020-03-31 16:52:20 +0530

    routes file fixed

---
+++ src/routes/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Registration from "./Registration";
+import firebase from "../config/firebase";
+
+class Routes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {}
+    };
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: null });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/user-registration" component={Registration} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default Routes;
